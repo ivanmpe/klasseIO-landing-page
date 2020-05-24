7fe8393a61f142dd00c4baf5e22ac5d4f7fd3bae
Ivan Alves <ivan_alves94@hotmail.com>
2020-05-24 11:04:04 -0300

    correcao server.js

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const path = require('path');
 const app = express();
 
 // Serve os arquivos estáticos da pasta dist (gerada pelo ng build)
-app.use(express.static(__dirname + '/dist/klasseio'));
+app.use(express.static(__dirname + '/dist/landingpage-ind'));
 
 app.get('/*', function(req, res) {
-    res.sendFile(path.join(__dirname + '/dist/klasseio/index.html'));
+    res.sendFile(path.join(__dirname + '/dist/landingpage-ind/index.html'));
 });
 
 // Inicia a aplicação pela porta configurada
